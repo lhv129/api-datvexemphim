42238ebad5f66bafc91acf2e8a2f71b97f02648e
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-01-20 22:11:08 +0700

    add table (products,movies,genres)

---
+++ database/migrations/2025_01_20_144156_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->text('description');
+            $table->string('poster',255)->nullable();
+            $table->string('trailer',255);
+            $table->integer('duration');
+            $table->decimal('rating', 2, 1);
+            $table->date('release_date');
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
