67bbf72815b981cd91388cb4d72f2ecf035aa97b
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-02-01 05:24:24 +0700

    update MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -16,7 +16,25 @@ class MovieController extends Controller
     {
         $movies = Movie::select('id', 'title', 'description', 'poster', 'fileName', 'trailer', 'duration', 'rating', 'release_date')
             ->get();
-        return $this->responseCommon(200, "Lấy danh sách phim thành công.", $movies);
+
+        $movie_genres = $movies->map(function ($movie) {
+            return [
+                'id' => $movie->id,
+                'genres' => Movie_genre::select('movie_genres.genre_id', 'genres.name')
+                    ->join('genres', 'genres.id', 'movie_genres.genre_id')
+                    ->where('movie_genres.movie_id', $movie->id)
+                    ->get(),
+                'title' => $movie->title,
+                'description' => $movie->description,
+                'poster' => $movie->poster,
+                'fileName' => $movie->fileName,
+                'trailer' => $movie->trailer,
+                'duration' => $movie->duration,
+                'rating' => $movie->rating,
+                'release_date' => $movie->release_date,
+            ];
+        });
+        return $this->responseCommon(200, "Lấy danh sách phim thành công.", $movie_genres);
     }
 
     public function store(Request $request)
@@ -61,8 +79,8 @@ class MovieController extends Controller
 
                 // Khi thêm phim thì cũng phải thêm thể loại cho phim đó.
                 // Lấy ra id của phim vừa thêm (mới nhất)
-                $latestIdMovie = Movie::orderBy('id','desc')->first()->id;
-                foreach($request->genres as $genre){
+                $latestIdMovie = Movie::orderBy('id', 'desc')->first()->id;
+                foreach ($request->genres as $genre) {
                     Movie_genre::create([
                         'movie_id' => $latestIdMovie,
                         'genre_id' => $genre
@@ -86,6 +104,10 @@ class MovieController extends Controller
             if ($validator->fails()) {
                 return $this->responseValidate(422, 'Dữ liệu không hợp lệ', $validator->errors());
             } else {
+                $genresExist = Genre::whereIn('id', $request->genres)->count() == count($request->genres);
+                if (!$genresExist) {
+                    return $this->responseCommon(400, "Một hoặc nhiều thể loại không tồn tại.", []);
+                }
                 if ($request->hasFile('poster')) {
                     $file = $request->file('poster');
                     // Đường dẫn ảnh
@@ -111,6 +133,16 @@ class MovieController extends Controller
                     'rating' => $request->rating,
                     'release_date' => $request->release_date,
                 ]);
+                $movie['genres'] = $request->genres;
+                //Xóa toàn bộ thể loại phim và thêm lại thể loại cho phim đó dựa theo update
+                Movie_genre::where('movie_id',$id)->delete();
+                foreach ($request->genres as $genre) {
+                    Movie_genre::create([
+                        'movie_id' => $id,
+                        'genre_id' => $genre
+                    ]);
+                }
+
                 return $this->responseCommon(200, "Cập nhật phim thành công.", $movie);
             }
         } catch (\Exception $e) {
@@ -122,6 +154,10 @@ class MovieController extends Controller
     {
         try {
             $movie = Movie::findOrFail($id);
+            $movie['genres'] = Movie_genre::select('movie_genres.genre_id', 'genres.name')
+                ->join('genres', 'genres.id', 'movie_genres.genre_id')
+                ->where('movie_genres.movie_id', $id)
+                ->get();
             return $this->responseCommon(200, "Tìm phim thành công.", $movie);
         } catch (\Exception $e) {
             return $this->responseCommon(404, "Phim này không tồn tại hoặc đã bị xóa.", []);
@@ -172,6 +208,7 @@ class MovieController extends Controller
             'description.min' => 'Mô tả phim phải ít nhất 5 kí tự.',
             'description.max' => 'Mô tả phim quá dài.',
             'mimes' => 'Bạn chỉ được nhập file ảnh có đuôi jpeg,jpg,png',
+            'genres.array' => 'Thể loại phim phải là 1 mảng array',
         ];
     }
 
@@ -185,6 +222,7 @@ class MovieController extends Controller
             'duration' => 'required',
             'rating' => 'required',
             'release_date' => 'required',
+            'genres' => 'required|array ',
         ];
     }
 
@@ -198,6 +236,7 @@ class MovieController extends Controller
             'description.min' => 'Mô tả phim phải ít nhất 5 kí tự.',
             'description.max' => 'Mô tả phim quá dài.',
             'mimes' => 'Bạn chỉ được nhập file ảnh có đuôi jpeg,jpg,png',
+            'genres.array' => 'Thể loại phim phải là 1 mảng array',
         ];
     }
 }
