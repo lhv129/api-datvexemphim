5f1af972dd68c4617a907007b51c877506f2cc96
DESKTOP-7Q3QPPV\ninhd <ninhduy147@gmail.com>
2025-03-14 21:16:55 +0700

    fix bug

--- app/Http/Controllers/ShowtimeController.php
+++ app/Http/Controllers/ShowtimeController.php
@@ -50,9 +50,7 @@ public function store(StoreShowtimeRequest $request)
         try {
             $validatedData = $request->validated();
 
-            // Chuyển đổi ngày giờ từ định dạng d/m/Y H:i sang Y-m-d H:i:s
-            // $validatedData['start_time'] = Carbon::createFromFormat('d/m/Y H:i', $validatedData['start_time'])->format('Y-m-d H:i:s');
-            // $validatedData['end_time'] = Carbon::createFromFormat('d/m/Y H:i', $validatedData['end_time'])->format('Y-m-d H:i:s');
+            // Chuyển đổi ngày giờ từ định dạng  H:i sang  H:i:s
 
             $validatedData['start_time'] = Carbon::createFromFormat('H:i', $validatedData['start_time'])->format('H:i:s');
             $validatedData['end_time'] = Carbon::createFromFormat('H:i', $validatedData['end_time'])->format('H:i:s');
